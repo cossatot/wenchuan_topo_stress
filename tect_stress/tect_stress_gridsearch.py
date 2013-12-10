25599d1e4168f7a6812dc295dedd707cadd5e4d3
richard styron <richard.h.styron@gmail.com>
2013-12-10 09:49:11 -0500

    more work on tect stress gridsearch

--- tect_stress/tect_stress_gridsearch.py
+++ tect_stress/tect_stress_gridsearch.py
@@ -4,6 +4,7 @@ sys.path.append('/cmld/data2/styron/src/halfspace/')
 import numpy as np
 import pandas as pd
 import halfspace.projections as hsp
+import time
 
 
 # Load fault slip and stress data
@@ -33,6 +34,7 @@ ind_array = np.array(new_index, dtype='int')
 
 # Make dataframe and fill in
 search_df_cols = ['iter', 'tect_xx', 'tect_yy', 'tect_xy', 'pt_index',
+                  'lat', 'lon', 'depth', 'strike', 'dip',
                   'topo_xx', 'topo_yy', 'topo_xy', 'topo_zz',
                   'topo_xz', 'topo_yz', 'slip_m', 'slip_rake',
                   'tau_rake', 'tau_mag', 'misfit', 'weighted_misfit']
@@ -42,17 +44,46 @@ search_df = pd.DataFrame(index=np.arange(len(new_index)),
 
 search_df[search_df_cols[:5]] = ind_array
 
+# Make a function to fill in the row values
 def fill_row_vals(row, lms_df=lms_df):
     pt = row['pt_index']
     
-    ser = pd.Series(index = ['topo_xx', 'topo_yy', 'topo_xy', 'topo_zz',
+    ser = pd.Series(index = ['lat', 'lon', 'depth', 'strike', 'dip',
+                             'topo_xx', 'topo_yy', 'topo_xy', 'topo_zz',
                              'topo_xz', 'topo_yz', 'slip_m', 'slip_rake'],
-                    data = lms_df[['xx_stress', 'yy_stress', 'xy_stress',
+                    data = lms_df[['lat_deg', 'lon_deg', 'depth_km',
+                                   'strike_deg', 'dip_deg',
+                                   'xx_stress', 'yy_stress', 'xy_stress',
                                    'zz_stress', 'slp_am_m','xz_stress',
-                                  'yz_stress', 'rake_deg']].iloc[pt].values )
+                                   'yz_stress', 'rake_deg']].iloc[pt].values )
     return ser
 
-search_df['topo_xx', 'topo_yy', 'topo_xy', 'topo_zz',
-          'topo_xz', 'topo_yz', 'slip_m', 'slip_rake'] = search_df.apply(
+print 'filling rows'
+t0 = time.time()
+search_df[['lat', 'lon', 'depth', 'strike', 'dip',
+           'topo_xx', 'topo_yy', 'topo_xy', 'topo_zz',
+           'topo_xz', 'topo_yz', 'slip_m', 'slip_rake']] = search_df.apply(
                                                         fill_row_vals, axis=1)
- 
+t1 = time.time()       
+print 'done in', t1 -t0, 's'
+
+# make a function to calculate the total stresses
+# try to do one thing at a time to cut down ram usage (experiment)
+def calc_tau(row):
+    T = hsp.make_xyz_stress_tensor(sig_xx = row['topo_xx'] + row['tect_xx'],
+                                   sig_yy = row['topo_yy'] + row['tect_yy'],
+                                   sig_xy = row['topo_xy'] + row['tect_xy'],
+                                   sig_zz = row['topo_zz'],
+                                   sig_xz = row['topo_xz'],
+                                   sig_yz = row['topo_yz'] )
+
+    tau_m, rake = hsp.max_shear_stress_from_xyz(strike=row['strike'],
+                                             dip = row['dip'],  stress_tensor=T)
+
+    return pd.Series({'tau_rake':rake, 'tau_mag':tau_m})
+
+print 'calculating tau' 
+search_df[['tau_rake', 'tau_mag']] = search_df.apply(calc_tau, axis=1)
+print 'done in', time.time() - t1, 's'
+
+
