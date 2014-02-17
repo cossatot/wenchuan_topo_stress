3cfb0c848a52c46ce6c2e1a0f9c5f38be6d9c946
richard styron <richard.h.styron@gmail.com>
2014-02-17 12:11:55 -0500

    updated zhang scripts for burgers with new rake calculations and an up-to-date failure script

--- tect_stress/scripts/zhang_bayes_fail.py
+++ tect_stress/scripts/zhang_bayes_fail.py
@@ -1,16 +1,17 @@
 import numpy as np
 import pandas as pd
 import stress_comps_vectorized as scv
+import halfspace.projections as hsp
 import time
 
 t0 = time.time()
-l = 2
 
-outfile = '../results/zhang_l{}_fail_posteriors.csv'.format(l)
-t_poster_file = '../results/zhang_l{}_tect_posteriors.csv'.format(l)
+outfile = '../results/zhang_fail_posteriors.csv'
+t_poster_file = '../results/zhang_tect_posteriors.csv'
 
 lms = pd.read_csv('../../slip_models/zhang/lms_stress_slip.csv', index_col=0)
-
+lms.rake_deg = hsp.get_rake_from_shear_components(strike_shear=lms.slp_strk_m,
+                                                  dip_shear=lms.slp_ddip_m)
 np.random.seed(69)
 
 t_prior_df = pd.read_csv(t_poster_file)
@@ -23,8 +24,8 @@ n_points = len(lms.index)
 rho = 2700
 g = 9.81
 
-# priors for friction (mu) and pore fluid pressure (lamb[da])
-mu_lamb_priors = np.random.random([n_trials, 2])
+# priors for pore fluid pressure (lamb[da])
+lamb_priors = np.random.random(n_trials)
 
 # make dataframe
 search_df_cols = ['iter', 'txx', 'tyy', 'txy', 'mu', 'lamb', 'pt_index', 
@@ -34,24 +35,24 @@ iter_range = np.arange(n_trials, dtype='float')
 pt_range = np.arange(n_points, dtype='float')
 
 # make list/array of sets of prior values for each fault point
-index_list = [[iter_range[i], mu_lamb_priors[i,0], mu_lamb_priors[i,1],
-               t_priors[i,0], t_priors[i,1], t_priors[i,2], pi]
+index_list = [[iter_range[i], lamb_priors[i], t_priors[i,0], 
+               t_priors[i,1], t_priors[i,2], pi]
               for i in iter_range for pi in pt_range]
 
 index_array = np.array(index_list)
 del index_list
 
 iter_index = np.int_(index_array[:,0].copy() )
-pt_index = np.int_(index_array[:,6].copy() )
-mu_lamb_prior_array = index_array[:,1:3]
-t_prior_array = index_array[:,3:6]
+pt_index = np.int_(index_array[:,5].copy() )
+mu_lamb_prior_array = index_array[:,1]
+t_prior_array = index_array[:,2:5]
 del index_array
 
 search_df = pd.DataFrame(index=np.arange(len(iter_index)),
                          columns=search_df_cols, dtype=float)
 
 search_df['iter'] = iter_index
-search_df[['mu', 'lamb']] = mu_lamb_prior_array
+search_df['lamb'] = lamb_prior_array
 search_df['pt_index'] = pt_index
 search_df[['txx', 'tyy', 'txy']] = t_prior_array
 
@@ -108,33 +109,23 @@ search_df['tau_mag'] = np.sqrt(search_df.tau_s**2 + search_df.tau_d**2)
 # calculate weighted misfits, start filtering
 mean_slip = lms.slp_am_m.mean()
 
-search_df['weighted_fail_misfit'] = (np.abs( search_df.tau_mag
-                                          - search_df.mu * search_df.sig_n_eff)
-                                     * (search_df.slip_m / mean_slip) )
+search_df['weighted_tau_misfit']=search_df.tau_mag *search_df.slip_m /mean_slip
 
 iters = search_df.groupby('iter')
-def l2_norm(array):
-    return np.linalg.norm(array, 2)
 
-fail_misfit = iters.weighted_fail_misfit.apply(l2_norm)
-fail_likelihood = 1 / (fail_misfit / fail_misfit.min() )
+mu_iter = iters.weighted_tau_mag.mean() / iters.sig_n_eff.mean()
+mu_real = mu_iter[(0 <= mu_iter) & (mu_iter <= 1)]
 
-rand_filter = np.random.random(n_trials)
-keeps = fail_likelihood[fail_likelihood**3 >= rand_filter] # rejection sample
+lamb_iters = iters.lamb.mean()
+lamb_keep = lamb_iters[mu_real.index]
 
-iters_txx = iters.txx.mean()
-iters_tyy = iters.tyy.mean()
-iters_txy = iters.txy.mean()
-iters_mu = iters.mu.mean()
-iters_lamb = iters.lamb.mean()
 
-txx_keep = iters_txx[keeps.index]
-tyy_keep = iters_tyy[keeps.index]
-txy_keep = iters_txy[keeps.index]
-mu_keep = iters_mu[keeps.index]
-lamb_keep = iters_lamb[keeps.index]
+txx_keep = iters.txx.mean()[mu_real.index]
+tyy_keep = iters.tyy[mu_real.index]
+txy_keep = iters.txy[mu_real.index]
 
-fail_posteriors = pd.concat([txx_keep, tyy_keep, txy_keep, mu_keep, lamb_keep],
+
+fail_posteriors = pd.concat([txx_keep, tyy_keep, txy_keep, mu_real, lamb_keep],
                             names = ['txx','tyy','txy','mu','lamb'])
 
 fail_posteriors.to_csv(outfile, index=False)
